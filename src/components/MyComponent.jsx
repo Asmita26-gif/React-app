a4a4308d4b618883ce0b372915a532bc23360b80
Asmita Pariyar <pariyarasmita319@gmail.com>
2025-03-24 21:10:30 -0700

    first commit

---
+++ src/components/MyComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Tailwind from './Tailwind'
+import DashBoard from './Dashboard'
+import Dashboard from './Dashboard'
+
+export default function MyComponent() {
+  return (
+    <div>
+      {/* <Tailwind /> */}
+      <Dashboard />
+    </div>
+  )
+}
+
